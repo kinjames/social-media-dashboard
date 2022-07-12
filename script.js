d2394957c61ac4fb2f82380318a867c756f45479
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-07-12 22:36:50 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 
-// const dark = document.querySelector('.switch');
-
-// dark.onclick = function (){
-//     document.body.classList.toggle('light-theme')
-// }
 
 document.querySelector('.switch')
     .addEventListener('mouseup', function() {
         document.body.classList.toggle('light-theme')
-    });
+});
